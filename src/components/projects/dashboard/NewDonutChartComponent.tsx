2cee882a0a0cd2ad179cd7507a62f24c81cde99d
Eduardo <131834306+EduardoR22@users.noreply.github.com>
2024-11-12 17:30:24 -0600

    Fixed that the supplier payment cost report did not print in pdf format, (work is being done on modifying the client donut-type graph in the project dashboard)

--- src/components/projects/dashboard/NewDonutChartComponent.tsx
+++ src/components/projects/dashboard/NewDonutChartComponent.tsx
@@ -1,12 +1,19 @@
 'use client'
 import { Doughnut } from 'react-chartjs-2';
-import { Chart, registerables, ArcElement } from "chart.js";
+import { Chart, registerables, ArcElement, defaults } from "chart.js";
 //Chart.register(...registerables);
 Chart.register(ArcElement);
 import { DonutChartJS } from '@/interfaces/DashboardProjects';
 
+// defaults.maintainAspectRatio = false;
+// defaults.responsive = true;
+
+// defaults.plugins.title.display = true;
+// defaults.plugins.title.align = 'start';
+// defaults.plugins.title.color = 'black';
+
 export default function NewDonutChartComponent({data}: {data: DonutChartJS}) {
-  // const data = {
+  // const dataP = {
   //   labels: [
   //     'Red',
   //     'Blue',
@@ -24,9 +31,66 @@ export default function NewDonutChartComponent({data}: {data: DonutChartJS}) {
   //   }]
   // };
 
+  // const dataJ = [{
+  //   "label": "title 1",
+  //   "value": 1
+  // }, 
+  // {
+  //   "label": "title 2",
+  //   "value": 2
+  // },
+  // {
+  //   "label": "title 3",
+  //   "value": 3
+  // }];
+
+  // const dataP = {
+  //   labels: dataJ.map((d) => d.label),
+  //   datasets: [
+  //     {
+  //       label: "count", 
+  //       data: dataJ.map((d) => d.value),
+  //       backgroundColor: [
+  //         'rgb(255, 99, 132)',
+  //         'rgb(54, 162, 235)',
+  //         'rgb(255, 205, 86)',
+  //       ],
+  //       borderColor: [
+  //         'rgb(255, 99, 132)',
+  //         'rgb(54, 162, 235)',
+  //         'rgb(255, 205, 86)',
+  //       ],
+  //       borderRadious: 5
+  //     },
+  //   ],
+  // }
+
+  const newData = {
+    labels: ['data 1', 'data 2', 'data 3'],
+    datasets: [
+      {
+        label: 'Projectos por cliente',
+        data: [1, 2, 3],
+        backgroundColor: [
+          'rgb(255, 99, 132)',
+          'rgb(54, 162, 235)',
+          'rgb(255, 205, 86)',          
+        ],
+        hoverOffset: 4
+      }
+    ]
+  };
+
   return (
     <>
       <Doughnut data={data} />
+      {/* <Doughnut data={newData} options={{
+        plugins: {
+          title: {
+            text: "titulo"
+          }
+        }
+      }} /> */}
     </>
   )
 }
