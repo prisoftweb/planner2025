87b1b2ce55b82e51923c541474eb840bcd0ad0d0
Eduardo <131834306+EduardoR22@users.noreply.github.com>
2026-02-19 16:47:18 -0600

    Responsive design was added to the departments section

--- src/components/departments/TableDepartments.tsx
+++ src/components/departments/TableDepartments.tsx
@@ -11,6 +11,8 @@ import { Options } from "@/interfaces/Common";
 import {Tooltip} from "@nextui-org/react";
 import ContainerSideNav from "../ContainerSideNav";
 import { propsTooltip } from "@/libs/animations";
+import { useTableStates } from "@/app/store/tableStates";
+import { useMemo } from "react";
 
 type DeptProps={
   data:DepartmentTable[], 
@@ -96,7 +98,87 @@ export default function TableDepartments({data, token, optionsCompany}: DeptProp
           <NewDepartment token={token} OptionsCompany={optionsCompany} dept={deptEdit || ''} showForm={setEditDept} />
         </ContainerSideNav>
       )}
-      <Table columns={columns} data={data} placeH="Buscar departamento.." />
+      <div className="hidden md:block w-full">
+        <Table columns={columns} data={data} placeH="Buscar departamento.." />
+      </div>
+      <div className="block md:hidden w-full">
+        <ListData data={data} token={token} />
+      </div>
     </>
+  )
+}
+
+const ListData = ({data, token}: {data: DepartmentTable[], token:string}) => {
+
+  // const [dataReports, setDataReports] = useState(data);
+  const {search} = useTableStates();
+
+  const filterData = useMemo(() => {
+    if(search.trim() === ''){
+      return data;
+    }else{
+      const d = data.filter(item => item.name.toLowerCase().includes(search.toLowerCase()));
+      return d;
+    }
+  }, [search]);
+
+  return(
+    <div>
+      <div className="relative flex flex-col text-gray-700 bg-white shadow-md w-full rounded-xl bg-clip-border] h-[450px]">
+        <nav className="flex w-full flex-col gap-1 p-2 font-sans text-base font-normal text-blue-gray-700
+          overflow-scroll overflow-y-auto overflow-x-hidden" style={{scrollbarColor: '#ada8a8 white', scrollbarWidth: 'thin'}}>
+
+          {filterData.map((d) => (
+            <CardDepartment department={d} key={d.id} token={token} />
+          ))}
+
+        </nav>
+      </div>
+    </div>
+  )
+}
+
+const CardDepartment = ({department, token}: 
+  {department:DepartmentTable, token:string}) => {
+  
+  return(
+    <div role="button"
+      key={department.id}
+      // onClick={() => window.location.replace(`/reports/${report.id}/profile`)}
+      className={`flex items-center justify-between w-full p-3 leading-tight transition-all rounded-lg 
+        outline-none text-start hover:bg-blue-gray-50 hover:bg-opacity-80 hover:text-blue-gray-900 
+        focus:bg-blue-gray-50 focus:bg-opacity-80 focus:text-blue-gray-900 active:bg-blue-gray-50 
+        active:bg-opacity-80 active:text-blue-gray-900 border-b border-slate-300 
+        bg-white`}
+    >
+      <div className="flex items-center w-full ">
+        <div className="grid mr-4 place-items-center">
+          <img alt="responsable" src={ department.company.logo ?? '/img/users/default.jpg'}
+            className="relative inline-block h-12 w-12 !rounded-full  object-cover object-center" />
+          <DeleteElement id={department.id} name={department.name} remove={RemoveDepartment} token={token} />
+        </div>
+        <div className="w-full">
+          <div className="flex gap-x-3 w-full justify-between items-center p-3">
+            <div>
+              <h6
+                className="block font-sans text-sm antialiased font-semibold leading-relaxed tracking-normal text-gray-600 ">
+                {department.name}
+              </h6>
+              <p className="block font-sans text-sm antialiased font-normal leading-normal text-gray-600">
+                {department.abbreviation}
+              </p>
+            </div>
+            {/* <div className="text-right">
+              <p className="block font-sans text-2xl antialiased font-normal leading-normal text-blue-600">
+                
+              </p>
+              <p className="block font-sans text-xs antialiased font-normal leading-normal text-gray-600">
+                
+              </p>
+            </div> */}
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
