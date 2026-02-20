00561f2db031dbcd5385bcef23aa675acfb32d14
Eduardo <131834306+EduardoR22@users.noreply.github.com>
2026-02-19 18:01:36 -0600

    Responsive design was added to the glossary section

--- src/components/glossary/TableGlossary.tsx
+++ src/components/glossary/TableGlossary.tsx
@@ -15,6 +15,8 @@ import {Tooltip} from "@nextui-org/react";
 import TooltipPencilIcon from "../tooltipIcons/TooltipPencilIcon";
 import ContainerSideNav from "../ContainerSideNav";
 import { propsTooltip } from "@/libs/animations";
+import { useTableStates } from "@/app/store/tableStates";
+import { useMemo } from "react";
 
 type tableProps={
   data:GlossaryTable[], 
@@ -136,7 +138,87 @@ export default function TableLists({data, token, glossaries}: tableProps){
           <NewGlossary token={token} glossary={glossEdit || ''} showForm={handleEditGloss} />
         </ContainerSideNav>
       )}
-      <Table columns={columns} data={table} placeH="Buscar glosario.." />
+      <div className="hidden md:block w-full">
+        <Table columns={columns} data={table} placeH="Buscar glosario.." />
+      </div>
+      <div className="block md:hidden w-full">
+        <ListData data={data} token={token} delGlossary={delGlossary} />
+      </div>
     </>
+  )
+}
+
+const ListData = ({data, token, delGlossary}: 
+  {data: GlossaryTable[], token:string, delGlossary: (id: string) => Promise<void>}) => {
+
+  // const [dataReports, setDataReports] = useState(data);
+  const {search} = useTableStates();
+
+  const filterData = useMemo(() => {
+    if(search.trim() === ''){
+      return data;
+    }else{
+      const d = data.filter(item => item.name.toLowerCase().includes(search.toLowerCase()));
+      return d;
+    }
+  }, [search]);
+
+  return(
+    <div>
+      <div className="relative flex flex-col text-gray-700 bg-white shadow-md w-full rounded-xl bg-clip-border] h-[450px]">
+        <nav className="flex w-full flex-col gap-1 p-2 font-sans text-base font-normal text-blue-gray-700
+          overflow-scroll overflow-y-auto overflow-x-hidden" style={{scrollbarColor: '#ada8a8 white', scrollbarWidth: 'thin'}}>
+
+          {filterData.map((g) => (
+            <CardGlosary glossary={g} key={g.id} delGlossary={delGlossary} token={token} />
+          ))}
+
+        </nav>
+      </div>
+    </div>
+  )
+}
+
+const CardGlosary = ({glossary, token, delGlossary}: 
+  {glossary:GlossaryTable, token:string, delGlossary: (id: string) => Promise<void>}) => {
+  
+  return(
+    <div role="button"
+      key={glossary.id}
+      className={`flex items-center justify-between w-full p-3 leading-tight transition-all rounded-lg 
+        outline-none text-start hover:bg-blue-gray-50 hover:bg-opacity-80 hover:text-blue-gray-900 
+        focus:bg-blue-gray-50 focus:bg-opacity-80 focus:text-blue-gray-900 active:bg-blue-gray-50 
+        active:bg-opacity-80 active:text-blue-gray-900 border-b border-slate-300 
+        bg-white`}
+    >
+      <div className="flex items-center w-full ">
+        <div className="grid mr-4 place-items-center">
+          {/* <img alt="responsable" src={ company.logo ?? '/img/users/default.jpg'}
+            className="relative inline-block h-12 w-12 !rounded-full  object-cover object-center" /> */}
+          <div className="w-8 h-8" style={{backgroundColor: glossary.color}}></div>
+          <RemoveElement id={glossary.id} name={glossary.name} token={token} 
+              remove={RemoveGlossary} removeElement={delGlossary} />
+        </div>
+        <div className="w-full">
+          <div className="flex gap-x-3 w-full justify-between items-center p-3">
+            <div>
+              <h6
+                className="block font-sans text-sm antialiased font-semibold leading-relaxed tracking-normal text-gray-600 ">
+                {glossary.name}
+              </h6>
+              <p className="block font-sans text-sm antialiased font-normal leading-normal text-gray-600">
+                {glossary.description}
+              </p>
+            </div>
+            {/* <div className="text-right">
+              <p className="block font-sans text-2xl antialiased font-normal leading-normal text-blue-600">
+              </p>
+              <p className="block font-sans text-xs antialiased font-normal leading-normal text-gray-600">
+              </p>
+            </div> */}
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
