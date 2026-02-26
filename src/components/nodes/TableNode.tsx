1edca4198cd065bdafc0bf9817abdaac9857948b
Eduardo <131834306+EduardoR22@users.noreply.github.com>
2026-02-26 16:18:15 -0600

    add responsive design in nodes section

--- src/components/nodes/TableNode.tsx
+++ src/components/nodes/TableNode.tsx
@@ -2,13 +2,14 @@
 import { createColumnHelper } from "@tanstack/react-table";
 import Table from "@/components/Table";
 import DeleteElement from "../DeleteElement";
-import { useState } from "react";
+import { useState, useMemo } from "react";
 import { removeNode } from "@/app/api/routeNodes";
 import { NodeTable } from "@/interfaces/Nodes";
 import UpdateNode from "./UpdateNode";
 import { Options } from "@/interfaces/Common";
 import TooltipPencilIcon from "../tooltipIcons/TooltipPencilIcon";
 import ContainerSideNav from "../ContainerSideNav";
+import { useTableStates } from "@/app/store/tableStates";
 
 type Params = {
   data:NodeTable[], 
@@ -98,7 +99,88 @@ export default function TableNode({data, token, departments, glossaries, workflo
             optDesc={optDesc} optRels={optRels} node={nodeEdit} />
         </ContainerSideNav>
       ) }
-      <Table columns={columns} data={data} placeH="Buscar nodo.." />
+      <div className="hidden md:block w-full">
+        <Table columns={columns} data={data} placeH="Buscar nodo.." />
+      </div>
+      <div className="block md:hidden w-full">
+        <ListData data={data} token={token} />
+      </div>
     </>
+  )
+}
+
+const ListData = ({data, token}: {data: NodeTable[], token:string}) => {
+
+  // const [dataReports, setDataReports] = useState(data);
+
+  const {search} = useTableStates();
+
+  const filterData = useMemo(() => {
+    if(search.trim() === ''){
+      return data;
+    }else{
+      const d = data.filter(item => item.caminos.toLowerCase().includes(search.toLowerCase()));
+      return d;
+    }
+  }, [search]);
+
+  return(
+    <div>
+      <div className="relative flex flex-col text-gray-700 bg-white shadow-md w-full rounded-xl bg-clip-border] h-[450px]">
+        <nav className="flex w-full flex-col gap-1 p-2 font-sans text-base font-normal text-blue-gray-700
+          overflow-scroll overflow-y-auto overflow-x-hidden" style={{scrollbarColor: '#ada8a8 white', scrollbarWidth: 'thin'}}>
+
+          {filterData.map((c) => (
+            <CardNode node={c} key={c.id} token={token} />
+          ))}
+
+        </nav>
+      </div>
+    </div>
+  )
+}
+
+const CardNode = ({node, token}: 
+  {node:NodeTable, token:string}) => {
+  
+  return(
+    <div role="button"
+      key={node.id}
+      // onClick={() => window.location.replace(`/reports/${report.id}/profile`)}
+      className={`flex items-center justify-between w-full p-3 leading-tight transition-all rounded-lg 
+        outline-none text-start hover:bg-blue-gray-50 hover:bg-opacity-80 hover:text-blue-gray-900 
+        focus:bg-blue-gray-50 focus:bg-opacity-80 focus:text-blue-gray-900 active:bg-blue-gray-50 
+        active:bg-opacity-80 active:text-blue-gray-900 border-b border-slate-300 
+        bg-white`}
+    >
+      <div className="flex items-center w-full ">
+        <div className="grid mr-4 place-items-center">
+          <img alt="responsable" src={ '/img/catalogs.svg'}
+            className="relative inline-block h-12 w-12 !rounded-full  object-cover object-center" />
+          <DeleteElement id={node.id} name={node.department} remove={removeNode} token={token} />
+        </div>
+        <div className="w-full">
+          <div className="flex gap-x-3 w-full justify-between items-center p-3">
+            <div>
+              <h6
+                className="block font-sans text-sm antialiased font-semibold leading-relaxed tracking-normal text-gray-600 ">
+                {node.workflow}
+              </h6>
+              <p className="block font-sans text-sm antialiased font-normal leading-normal text-gray-600">
+                {node.caminos}
+              </p>
+            </div>
+            <div className="text-right">
+              <p className="block font-sans text-2xl antialiased font-normal leading-normal text-blue-600">
+                {node.department}  
+              </p>
+              <p className="block font-sans text-xs antialiased font-normal leading-normal text-gray-600">
+                {node.condition}
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
