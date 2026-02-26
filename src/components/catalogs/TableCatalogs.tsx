f318666b0958e50e77115caf600ed45bf903d229
Eduardo <131834306+EduardoR22@users.noreply.github.com>
2026-02-26 15:16:29 -0600

    add responsive design in list section

--- src/components/catalogs/TableCatalogs.tsx
+++ src/components/catalogs/TableCatalogs.tsx
@@ -2,7 +2,7 @@
 import { createColumnHelper } from "@tanstack/react-table";
 import Table from "@/components/Table";
 import { PencilIcon } from "@heroicons/react/24/solid";
-import { useState } from "react";
+import { useState, useMemo } from "react";
 import { CatalogTable } from "@/interfaces/Catalogs";
 import { RemoveCatalog } from "@/app/api/routeCatalogs";
 import NewCatalog from "./NewCatalog";
@@ -12,6 +12,7 @@ import { showToastMessageError } from "../Alert";
 import {Tooltip} from "@nextui-org/react";
 import ContainerSideNav from "../ContainerSideNav";
 import { propsTooltip } from "@/libs/animations";
+import { useTableStates } from "@/app/store/tableStates";
 
 export default function TableCatalogs({data, token}: {data:CatalogTable[], token:string}){
   
@@ -94,7 +95,90 @@ export default function TableCatalogs({data, token}: {data:CatalogTable[], token
           <NewCatalog token={token} catalog={catEdit || ''} showForm={setEditCat} />
         </ContainerSideNav>
       )}
-      <Table columns={columns} data={data} placeH="Buscar catalogo.." />
+      <div className="hidden md:block w-full">
+        <Table columns={columns} data={data} placeH="Buscar catalogo.." />
+      </div>
+      <div className="block md:hidden w-full">
+        <ListData data={data} token={token} delReport={delReport} />
+      </div>
     </>
+  )
+}
+
+const ListData = ({data, token, delReport}: 
+  {data: CatalogTable[], token:string, delReport: (id: string) => Promise<void>}) => {
+
+  // const [dataReports, setDataReports] = useState(data);
+
+  const {search} = useTableStates();
+
+  const filterData = useMemo(() => {
+    if(search.trim() === ''){
+      return data;
+    }else{
+      const d = data.filter(item => item.name.toLowerCase().includes(search.toLowerCase()));
+      return d;
+    }
+  }, [search]);
+
+  return(
+    <div>
+      <div className="relative flex flex-col text-gray-700 bg-white shadow-md w-full rounded-xl bg-clip-border] h-[450px]">
+        <nav className="flex w-full flex-col gap-1 p-2 font-sans text-base font-normal text-blue-gray-700
+          overflow-scroll overflow-y-auto overflow-x-hidden" style={{scrollbarColor: '#ada8a8 white', scrollbarWidth: 'thin'}}>
+
+          {filterData.map((c) => (
+            <CardCatalog company={c} key={c.id} token={token} delReport={delReport} />
+          ))}
+
+        </nav>
+      </div>
+    </div>
+  )
+}
+
+const CardCatalog = ({company, token, delReport}: 
+  {company:CatalogTable, token:string, delReport: (id: string) => Promise<void>}) => {
+  
+  return(
+    <div role="button"
+      key={company.id}
+      // onClick={() => window.location.replace(`/reports/${report.id}/profile`)}
+      className={`flex items-center justify-between w-full p-3 leading-tight transition-all rounded-lg 
+        outline-none text-start hover:bg-blue-gray-50 hover:bg-opacity-80 hover:text-blue-gray-900 
+        focus:bg-blue-gray-50 focus:bg-opacity-80 focus:text-blue-gray-900 active:bg-blue-gray-50 
+        active:bg-opacity-80 active:text-blue-gray-900 border-b border-slate-300 
+        bg-white`}
+    >
+      <div className="flex items-center w-full ">
+        <div className="grid mr-4 place-items-center">
+          <img alt="responsable" src={ '/img/catalog.svg'}
+            className="relative inline-block h-12 w-12 !rounded-full  object-cover object-center" />
+          <RemoveElement id={company.id} name={company.name} token={token} 
+              remove={RemoveCatalog} removeElement={delReport} />
+        </div>
+        <div className="w-full">
+          <div className="flex gap-x-3 w-full justify-between items-center p-3">
+            <div>
+              <h6
+                className="block font-sans text-sm antialiased font-semibold leading-relaxed tracking-normal text-gray-600 ">
+                {company.name}
+              </h6>
+              <p className="block font-sans text-sm antialiased font-normal leading-normal text-gray-600">
+                {company.collection}
+              </p>
+            </div>
+            {/* <div className="text-right">
+              <p className="block font-sans text-2xl antialiased font-normal leading-normal text-blue-600">
+                
+              </p>
+              <p className="block font-sans text-xs antialiased font-normal leading-normal text-gray-600">
+                {company.email}
+              </p>
+            </div> */}
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
